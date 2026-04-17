264bebbc686fd65859844556d9bc5ef619d4064d
Roberto Molowny-Horas <roberto@creaf.uab.es>
2026-04-17 16:32:12 +0200

    Try some combinations of predictors to model organic carbon. To be improved.

--- R/modelling_CO2.R
+++ R/modelling_CO2.R
@@ -2,10 +2,21 @@ modelling_CO2 <- function() {
 
   df <- blaucat_dat[, c("Carboni orgànic del sediment_mean",
                         "Ús del sòl del lloc de mostreig",
-                        "Distància al mar")]
+                        "Distància al mar",
+                        "Distància a zones agrícoles",
+                        "Distància a zones urbanes",
+                        "Temperatura mitjana de l'aire",
+                        "Precipitació acumulada",
+                        "Elevació respecte al nivell del mar",
+                        "Tipus de sòl")]
   df$`Ús del sòl del lloc de mostreig` <- factor(df$`Ús del sòl del lloc de mostreig`)
+  df$`Tipus de sòl` <- factor(df$`Tipus de sòl`)
 
+  df <- na.omit(df)
 
   m1 <- glm(`Carboni orgànic del sediment_mean` ~ ., data = df)
+  print(summary(m1))
+  m2 <- MASS::stepAIC(m1)
+  print(summary(m2))
 
 }
