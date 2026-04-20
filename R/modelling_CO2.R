90aea3f19828d1c02d80da734a00e88ac06f4092
Roberto Molowny-Horas <roberto@creaf.uab.es>
2026-04-20 17:18:18 +0200

    First attempt at modelling carbon.

--- R/modelling_CO2.R
+++ R/modelling_CO2.R
@@ -14,9 +14,40 @@ modelling_CO2 <- function() {
 
   df <- na.omit(df)
 
-  m1 <- glm(`Carboni orgànic del sediment_mean` ~ ., data = df)
+  df$`Distància al mar` <- log(df$`Distància al mar`)
+  df$`Distància a zones agrícoles` <- log(df$`Distància a zones agrícoles`)
+  df$`Distància a zones urbanes` <- log(df$`Distància a zones urbanes`)
+
+  m1 <- glm(`Carboni orgànic del sediment_mean` ~ ., data = df, family = Gamma(link = "log"))
   print(summary(m1))
   m2 <- MASS::stepAIC(m1)
   print(summary(m2))
+
+  print(performance::r2_mcfadden(m2))
+
+  #####
+  library(gamlss)
+
+  m3 <- gamlss(formula = `Carboni orgànic del sediment_mean` ~ . ,
+               sigma.formula = ~ 1,
+               data = df, family = LOGNO)
+  m4 <- MASS::stepAIC(m3)
+  print(summary(m3))
+  print(summary(m4))
+  print(cor(df$`Carboni orgànic del sediment_mean`, predict(m4)))
+  plot(df$`Carboni orgànic del sediment_mean`, predict(m4))
+
+  # Let us gamlss choose the best distribution.
+  best_dist <- chooseDist(m4, type = "realplus")
+  getOrder(best_dist)
+
+
+
+  df_clean <- df
+  names(df_clean) <- c("Carbon", "LandUs", "DistSea", "DistAgri", "DistUrba", "Temp", "Prec", "Elev", "Soil")
+  m3 <- gamlss(formula = Carbon ~ . ,
+               sigma.formula = ~ 1,
+               data = df_clean, family = LOGNO)
+
 
 }
